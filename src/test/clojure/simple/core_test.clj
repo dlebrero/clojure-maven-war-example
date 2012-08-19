a8053c7c86ba08e37db9eb9fbffdb6e368a1cecd
Daniel Lebrero <dlebrero@gmail.com>
2012-08-19 15:32:39 +0100

    adding some tests

---
+++ src/test/clojure/simple/core_test.clj
@@ -0,0 +1,12 @@
+(ns simple.core-test
+  (:require [simple.core :as sc])
+  (:use clojure.test
+        [ring.mock.request :only [request header]]
+        [midje.sweet :only [facts contains defchecker]]))
+
+(defchecker is-status [code]
+  (contains {:status code}))
+
+(deftest status (facts "Simple test"
+           (let [response (sc/app (request :get "/clojure-servlet/"))]
+             response => (is-status 200))))
